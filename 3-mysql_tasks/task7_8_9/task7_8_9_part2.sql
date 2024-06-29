37c8155e2f61bf2bb7938c199a0ebfa5e3c95b12
TimofeevDmitrii <dimatimoha2014@gmail.com>
2024-06-29 22:11:40 +0300

    through LIKE creation method of tables pack_animals and pet_animals were writen

--- 3-mysql_tasks/task7_8_9/task7_8_9_part2.sql
+++ 3-mysql_tasks/task7_8_9/task7_8_9_part2.sql
@@ -1,37 +1,21 @@
--- Варианты создания даблицы через SELECT (нужно заново прописывать многие ограничения)
 -- DROP TABLE pack_animals;
-CREATE TABLE pack_animals 
-SELECT
-	animal_name,
-    animal_kind,
-	birth_date,
-	commands
-FROM 
-(SELECT 'camel' AS animal_kind, animal_name, birth_date, commands FROM camels AS c
-UNION 
-SELECT 'horse' AS animal_kind, animal_name, birth_date, commands FROM horses AS h
+CREATE TABLE pack_animals LIKE camels;
+ALTER TABLE pack_animals
+ADD COLUMN animal_kind VARCHAR(30) NOT NULL AFTER animal_name;
+INSERT INTO pack_animals (animal_name, animal_kind, birth_date, commands)
+(SELECT animal_name, 'camel' AS animal_kind, birth_date, commands FROM camels
 UNION
-SELECT 'donkey' AS animal_kind, animal_name, birth_date, commands FROM donkeys AS d) AS res;
-ALTER TABLE pack_animals 
-ADD COLUMN id SERIAL PRIMARY KEY FIRST,
-MODIFY COLUMN animal_kind VARCHAR(30) NOT NULL,
-ALTER COLUMN birth_date SET DEFAULT (CURRENT_DATE);
-
+SELECT animal_name, 'horse' AS animal_kind, birth_date, commands FROM horses
+UNION
+SELECT animal_name, 'donkey' AS animal_kind, birth_date, commands FROM donkeys);
 
 -- DROP TABLE pet_animals;
-CREATE TABLE pet_animals
-SELECT 
-	animal_name,
-    animal_kind,
-	birth_date,
-	commands
-FROM
-(SELECT 'cat' AS animal_kind, animal_name, birth_date, commands FROM cats AS c
+CREATE TABLE pet_animals LIKE dogs;
+ALTER TABLE pet_animals
+ADD COLUMN animal_kind VARCHAR(30) NOT NULL AFTER animal_name;
+INSERT INTO pet_animals (animal_name, animal_kind, birth_date, commands)
+(SELECT animal_name, 'cat' AS animal_kind, birth_date, commands FROM cats
 UNION 
-SELECT 'dog' AS animal_kind, animal_name, birth_date, commands FROM dogs AS d
+SELECT animal_name, 'dog' AS animal_kind, birth_date, commands FROM dogs 
 UNION
-SELECT 'hamster' AS animal_kind, animal_name, birth_date, commands FROM hamsters AS h) AS res;
-ALTER TABLE pet_animals 
-ADD COLUMN id SERIAL PRIMARY KEY FIRST,
-MODIFY COLUMN animal_kind VARCHAR(30) NOT NULL,
-ALTER COLUMN birth_date SET DEFAULT (CURRENT_DATE);
+SELECT animal_name, 'hamster' AS animal_kind, birth_date, commands FROM hamsters);
